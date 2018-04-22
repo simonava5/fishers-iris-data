be287e723a94d3679efe6ab502553bd0a0f94f17
tomasmurray <G00364832@gmit.ie>
2018-04-22 13:53:55 +0100

    update

--- fishers_iris_data.py
+++ fishers_iris_data.py
@@ -125,6 +125,7 @@ import seaborn as sns
 
 fig5 = sns.pairplot(data, hue='species', size=2) # hue identifies the class to colourise; size is height (in inches!) of each facet
 fig5.fig.subplots_adjust(right = 0.8) # There was a bug in seaborn as the legend is rendered over the pairplot, not outside to the centre right. The solution used on this line to adjust the right margin was posted here: https://stackoverflow.com/questions/37815774/seaborn-pairplot-legend-how-to-control-position
+
 plt.savefig('figures/fig5.jpg')
 
 # To test statistical differences between the species across the measured features, I wanted to repeat the ANOVA analyses in Fisher's original paper but from the figures it was clear that other than Fig. 2, the data was largely non-normal and heteroscedastic so I chose non-parametric tests. I could test for normality too (e.g. Shapiro-Wilk), but with a sample size of 50 across groups there may not be sufficient power to reject normality making the test meaningless.
@@ -134,6 +135,7 @@ plt.savefig('figures/fig5.jpg')
 import scipy.stats as sc
 
 # ensure species is being recogised as a categorical as it was originally called 'object' above: https://pandas.pydata.org/pandas-docs/stable/categorical.html 
+
 data['species'] = data['species'].astype('category')
 print(data.dtypes)
 
